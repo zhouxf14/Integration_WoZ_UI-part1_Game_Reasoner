ed1898fe4a64e907bd99167c939c20b03095c091
Steven Tang <767527493@qq.com>
2017-09-11 21:33:06 -0400

    no message

--- UI-part1_Game_Reasoner_Integration/src/edu/usc/ict/vhmsg/Config.java
+++ UI-part1_Game_Reasoner_Integration/src/edu/usc/ict/vhmsg/Config.java
@@ -8,7 +8,7 @@ public class Config {
 	//public static String VHMSG_SERVER_URL = "192.168.1.122";
 	//public static String VHMSG_SERVER_URL = "192.168.1.133";
 	//public static String VHMSG_SERVER_URL = "localhost";
-	public static String VHMSG_SERVER_URL = "128.237.190.220";
+	public static String VHMSG_SERVER_URL = "128.237.213.149";
 	
 	public static String accessKey = "GDNAJRE2KBPMBQCEU2CQ";
 	public static String secretKey = "rCDTav82VFzUsmPeSP8jxj+mlFAhhfCT9i71DCsm";
