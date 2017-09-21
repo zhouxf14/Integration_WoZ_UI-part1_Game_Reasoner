bef0204145f703b8613996dc9c4db5537b204755
Steven Tang <767527493@qq.com>
2017-09-20 20:59:14 -0400

    Code adjustment from IntelliJ to Eclipse

--- UI-part1_Game_Reasoner_Integration/src/edu/usc/ict/vhmsg/Config.java
+++ UI-part1_Game_Reasoner_Integration/src/edu/usc/ict/vhmsg/Config.java
@@ -7,8 +7,8 @@ public class Config {
 	//public static String VHMSG_SERVER_URL = "192.168.1.100";
 	//public static String VHMSG_SERVER_URL = "192.168.1.122";
 	//public static String VHMSG_SERVER_URL = "192.168.1.133";
-	//public static String VHMSG_SERVER_URL = "localhost";
-	public static String VHMSG_SERVER_URL = "128.237.194.149";
+	public static String VHMSG_SERVER_URL = "localhost";
+	//public static String VHMSG_SERVER_URL = "128.237.172.221";
 	
 	public static String accessKey = "GDNAJRE2KBPMBQCEU2CQ";
 	public static String secretKey = "rCDTav82VFzUsmPeSP8jxj+mlFAhhfCT9i71DCsm";
